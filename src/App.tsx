92b9ff5fcf572ab19634001b11e940d888c20d35
Marcos <mvmendesferreira@id.uff.br>
2025-08-07 14:21:12 -0300

    add: criando card da pessoa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header";
+import ProfileCard from "./components/ProfileCard";
+import fotoMarcos from './assets/foto_marcos.png';
 
 export default function App() {
 
   return (
     <>
       <Header />
+      <ProfileCard name="Marcos Ferreira" profession="Dev Front-end" profilePicture={fotoMarcos} backgroundPicture="https://imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2018/10/03/506414/20181003165936245931i.jpg" />
     </>
   )
 }
