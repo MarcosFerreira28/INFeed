29945381074c0b3f751ccf4ef8d450a1236fe89b
Marcos <mvmendesferreira@id.uff.br>
2025-08-07 11:47:30 -0300

    add: criando header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import style from './styles.module.css'
+import logoIN from '../../assets/Logo_1.svg'
 
 export default function App() {
     return (
         <div className={style.container}>
-            <img src="./logo.svg" alt="Logo" />
+            <img src={logoIN} alt="Logo" />
             <p className={style.feed}>Feed</p>
         </div>
     )
