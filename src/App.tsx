baa33c61e0b0a0a537fff8ac7ee065a75a7f3da2
Marcos <mvmendesferreira@id.uff.br>
2025-08-07 14:28:27 -0300

    fix: retirando os cards e colocando num componente separado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header/Header";
 import ProfileCard from "./components/ProfileCard";
 import fotoMarcos from './assets/foto_marcos.png';
+import Main from "./components/Main";
 
 export default function App() {
 
   return (
     <>
       <Header />
-      <ProfileCard name="Marcos Ferreira" profession="Dev Front-end" profilePicture={fotoMarcos} backgroundPicture="https://imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2018/10/03/506414/20181003165936245931i.jpg" />
+      <Main />
     </>
   )
 }
---
+++ src/components/Publicacao/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Publicacao() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
